d067452c3415302d114acb90caeac2c03cea00ee
esewho <albertogfcb@gmail.com>
2025-11-20 21:44:18 +0100

    feat: refactor profile controller to use UserId decorator for user identification

--- src/app/profile/profile.controller.ts
+++ src/app/profile/profile.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { UpdateProfileDto } from 'src/dtos/update-profile.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UserId } from '../common/decorators/user-id.decorator';
 
 @Controller('profile')
 export class ProfileController {
@@ -9,13 +10,16 @@ export class ProfileController {
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
-  async getMyProfile(userId: string) {
+  async getMyProfile(@UserId() userId: string) {
     return await this.profileService.getUserProfile(userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Put('me')
-  async updateMyProfile(userId: string, @Body() dto: UpdateProfileDto) {
+  async updateMyProfile(
+    @UserId() userId: string,
+    @Body() dto: UpdateProfileDto,
+  ) {
     return await this.profileService.updateUserProfile(userId, dto);
   }
 }
